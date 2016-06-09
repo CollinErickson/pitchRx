e65828e100d4235abb140a58dd608289b0f24e07
Collin Erickson <collinberickson@gmail.com>
2016-06-08 19:22:58 -0500

    Can't get all player stats by game, still looking around.

--- create_database.R
+++ create_database.R
@@ -2,16 +2,19 @@ library(dplyr)
 library("RSQLite", lib.loc="~/R/win-library/3.3")
 library(pitchRx)
 
-#my_db <- src_sqlite("Gameday.sqlite3")
+my_db <- src_sqlite("Gameday.sqlite3")
 #my_db <- src_sqlite("Gameday.sqlite3",create = T)
 
 #scrape(start = "2013-08-01", end = "2013-08-01", connect = my_db$con)
 
 files <- c("inning/inning_hit.xml", "miniscoreboard.xml", "players.xml")
-scrape(start = "2015-08-01", end = "2015-08-01", connect=my_db$con, suffix = files)
+#scrape(start = "2015-08-01", end = "2015-08-01", connect=my_db$con, suffix = files)
+#scrape(start = "2015-08-01", end = "2015-08-01", connect=my_db$con)
 
 my_db$info
 
 tbl(my_db,'hip')
 tbl(my_db,'player')
+tbl(my_db,'action')
+tbl(my_db,'atbat')
 # game, media, coach, player, umpire, hip
